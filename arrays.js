ddd74b4c701523fb11664e09fc3c2d3115d7d8b8
Rahulllkr <rahulkumar8604745@gmail.com>
2025-07-19 01:47:19 +0530

    practice

--- arrays.js
+++ arrays.js
@@ -1,2 +1,54 @@
-let a = new Array()
-console.log(a );
+//find the maximum number
+// let arr = [3,5,1,8,2]
+
+// function maximum(arr){
+//     let max = 0
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     console.log(max);
+
+// }
+
+// maximum(arr);
+
+
+//sum of all elements
+
+// let arr = [1,2,3,4,5]
+
+// function sum(arr){
+// let sum = 0;
+// for(let i = 0 ; i < arr.length ; i++){
+//     sum += arr[i]
+// }
+// console.log(sum)
+// }
+// sum(arr);
+
+
+
+//reverse an array
+
+let arr = [1,2,3,4]
+
+function reverse(arr){
+
+    let last =arr.length-1;
+    let first = 0;
+
+    while ( first < last){
+        [arr[first],arr[last]] = [
+            arr[last],arr[first]
+        ]
+          first++
+          last--
+    }
+
+    return arr;
+
+}
+
+console.log(reverse(arr));
